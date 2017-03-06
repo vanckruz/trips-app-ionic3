6efa21df9892d6a520bfa61dd0e895cc2aab97ec
Jhonny Vanckruz <jhonnyvanckruz@gmail.com>
2017-03-06 19:59:59 -0400

    Refactoring of code

--- src/pages/trips-guest/trips-guest.ts
+++ src/pages/trips-guest/trips-guest.ts
@@ -1,11 +1,12 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, PopoverController, NavParams, ModalController, ViewController, LoadingController } from 'ionic-angular';
+
+import { LoginPage } from '../login/login';
 import { DetailTrip } from '../detail-trip/detail-trip';
 import { SearchTrips } from '../search-trips/search-trips';
 import { FilterTrips } from '../filter-trips/filter-trips';
 import { SearchServices } from '../../providers/search.services';
 import { ActivitisServices } from '../../providers/activities.services';
-import { LoginPage } from '../login/login';
 import { TripServices } from '../../providers/trips.services';
 
 
@@ -33,97 +34,26 @@ export class TripsGuestPage {
 		public loading: LoadingController,
 		public tripServices: TripServices
 		){
-				this.trips = this.tripServices.getTrips();
-				this.getActivities();
+			//this.getTrips();
+			this.trips = this.tripServices.getTrips();
+			this.getActivities();
 	}
 
-	ionViewDidLoad() { }
-
 	getActivities(){
-			this.activitiesProvider.get()
-			.subscribe( (data) => {
-				this.activities = data;
-				console.log(this.activities)
-			});
+		this.activitiesProvider.get()
+		.subscribe( (data) => {
+			this.activities = data;
+		});
+	}
+
+	getTrips(){
+		//this.tripServices.getTrips().subscribe( data => this.trips = data );
 	}
 
 	detailTrip(trip){
 		this.navCtrl.push(DetailTrip,{
 			"trip":trip
 		});
-	}
-
-	showSearchModal(ev)
-	{	   
-	    let modal = this.modalCtrl.create(SearchTrips,{params: this.activities});
-	    modal.present();
-	    modal.onDidDismiss((data: any) => {
-
-				if(data.run) 
-				{
-					console.log("data desde la vista disparadora. ",data);
-							let loading = this.loading.create({
-								content: 'Please wait...'
-							});    
-							loading.present().then(() => {      
-										this.search.search(data).subscribe( (data) => {
-													loading.dismiss().then( () => {
-																console.log(data);
-																// this.trips = data;                    
-													} );
-										});
-							});//Loading   			
-				}
-
-	    });	  	      
-	}	
-
-	filterTrips(filterObject = null){
-
-		if(this.trips.lenght === 0 || filterObject === null ){
-			return this.trips;
-		}
-
-		if(filterObject !== null){
-							
-				let filter = this.trips.filter(
-					item => {
-						
-						if(item.genderDriver.toLowerCase() === filterObject.gender.toLowerCase()){
-							return true;
-						}
-						
-						return false;
-				});
-
-				return this.trips = filter;
-				
-		}
-		
-	}
-	
-
-	showFiltersModal(ev)
-	{	   
-	    let modal = this.modalCtrl.create(FilterTrips,{params: this.activities});
-	    modal.present();
-	    modal.onDidDismiss((data: any) => {
-
-				if(data.run) 
-				{
-						let loading = this.loading.create({
-							content: 'Please wait...'
-						});   
-
-						loading.present().then( () => {      
-							loading.dismiss().then( () => {
-									console.log(data);		        	
-									this.filterTrips(data);
-							});				
-						});//Loading   			
-				}
-
-	    });	 
 	}
 
 	refreshTrips(){
@@ -133,5 +63,75 @@ export class TripsGuestPage {
 	backTohome(){
     	this.navCtrl.setRoot(LoginPage);
 	}
+
+	showSearchModal(ev)
+	{	   
+	    let modal = this.modalCtrl.create(SearchTrips,{params: this.activities});
+	    modal.present();
+	    modal.onDidDismiss((data: any) => {
+
+			if(data.run) 
+			{
+				console.log("data desde la vista disparadora. ",data);
+				let loading = this.loading.create({
+					content: 'Please wait...'
+				});    
+				
+				loading.present().then(() => {      
+					this.search.search(data).subscribe( (data) => {
+						loading.dismiss().then( () => {
+							console.log(data);
+							this.trips = data;                    
+						} );
+					});
+				});//Loading   			
+			}
+
+	    });	  	      
+	}	
+	
+	showFiltersModal(ev)
+	{	   
+	    let modal = this.modalCtrl.create(FilterTrips,{params: this.activities});
+	    modal.present();
+	    modal.onDidDismiss((data: any) => {
+
+			if(data.run) 
+			{
+				let loading = this.loading.create({
+					content: 'Please wait...'
+				});   
+
+				loading.present().then( () => {      
+					loading.dismiss().then( () => {
+						console.log(data);		        	
+						this.filterTrips(data);
+					});				
+				});//Loading   			
+			}
+
+	    });	 
+	}
+
+	filterTrips(filterObject = null){
+
+		if(this.trips.lenght === 0 || filterObject === null ){
+			return this.trips;
+		}
+
+		if(filterObject !== null){
+							
+			let filter = this.trips.filter(
+				item => {						
+					if(item.genderDriver.toLowerCase() === filterObject.gender.toLowerCase()){
+						return true;
+					}
+					return false;
+				});
+
+			return this.trips = filter;				
+		}		
+	}	
+
 }
 
