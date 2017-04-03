a1c4494e621ce95538f2455c42db33d851232fac
Jhonny <jhonnyvanckruz@gmail.com>
2017-04-03 12:29:14 -0400

    quickly changes and update ionic and install plugin paypal

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,27 +3,34 @@ export var config = {
 			/*Get Routes*/
 			//Routes with tokens (Protected)
 			myTrips: function(parameters){
-				return `http://qa.ridj-it.com/app/api/mobile/V1/user/${parameters.id}/mytrips?token=${parameters.token}`
+				// return `http://qa.ridj-it.com/app/api/mobile/V1/user/${parameters.id}/mytrips?token=${parameters.token}`
+				return `http://ridj-it.getsandbox.com/app/api/mobile/V1/user/${parameters.id}/mytrips?token=${parameters.token}`
 			},
 			profile: function(parameters){
-				return `http://qa.ridj-it.com/app/api/mobile/V1/user/${parameters.id}/profile?token=${parameters.token}`
+				// return `http://qa.ridj-it.com/app/api/mobile/V1/user/${parameters.id}/profile?token=${parameters.token}`
+				return `http://ridj-it.getsandbox.com/app/api/mobile/V1/user/${parameters.id}/profile?token=${parameters.token}`
 			},
 			//Untokens (Routes publics)
 			activities: function(){
-				return `http://qa.ridj-it.com/app/api/mobile/V1/activities`
+				// return `http://qa.ridj-it.com/app/api/mobile/V1/activities`
+				return `http://ridj-it.getsandbox.com/app/api/mobile/V1/activities`
 			},
 			trips: function(){
-				return `http://qa.ridj-it.com/app/api/mobile/V1/trips`
+				// return `http://qa.ridj-it.com/app/api/mobile/V1/trips`
+				return `http://ridj-it.getsandbox.com/app/api/mobile/V1/trips`
 			},
 			search: function(){
-				return `http://qa.ridj-it.com/app/search`
+				// return `http://qa.ridj-it.com/app/search`
+				return `http://ridj-it.getsandbox.com/app/search`
 			},
 			/*Post Routes*/
 			login: function(){
-				return `http://qa.ridj-it.com/app/api/mobile/V1/login`
+				// return `http://qa.ridj-it.com/app/api/mobile/V1/login`
+				return `http://ridj-it.getsandbox.com/app/api/mobile/V1/login`
 			},
 			singup: function(){
-				return `http://qa.ridj-it.com/app/api/mobile/V1/signup`
+				// return `http://qa.ridj-it.com/app/api/mobile/V1/signup`
+				return `http://ridj-it.getsandbox.com/app/api/mobile/V1/signup`
 			}
 		}
 	}
