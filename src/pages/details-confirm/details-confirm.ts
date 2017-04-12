5829237180ff4507cd99bb19412dc73385378719
Jhonny Vanckruz <jhonnyvanckruz@gmail.com>
2017-04-11 21:08:57 -0400

    sum array and total in let group trip

--- src/pages/details-confirm/details-confirm.ts
+++ src/pages/details-confirm/details-confirm.ts
@@ -46,17 +46,28 @@ export class DetailsConfirmPage {
 		this.navCtrl.push(NewCarPage);		
 	}
 
-	sumExtraprice(price){
-		this.parameters.extraCost.push(price);
-		console.log(price);
-		let total = 0;
+	sumExtraprice(cost){
+		
+		if( this.parameters.extraCost.indexOf(cost) == -1 ){
+			
+			this.parameters.extraCost.push(cost);
 
+		}else{
+			
+			this.parameters.extraCost.splice(this.parameters.extraCost.indexOf(cost), 1);
+
+		} 
+
+		console.log( this.parameters.extraCost );
+
+		let total = 0;
 		for(let i = 0, len = this.parameters.extraCost.length; i < len; i++) {
-			total += this.parameters.extraCost[i];
-		}		
-		this.trip.priceAsDriver = this.trip.priceAsDriver + total
-		console.log(this.trip.priceAsDriver);
-		return this.trip.priceAsDriver;
+			total += this.parameters.extraCost[i].priceAmount;
+		}	
+
+		console.log(this.trip.groupTripInfo.priceAsDriver);
+
+		return this.parameters.total = total;
 	}
 
 	updateTotal(check){
@@ -68,32 +79,13 @@ export class DetailsConfirmPage {
 			PayPalEnvironmentProduction: "access_token$sandbox$v3s48nj697rqm75b$5411d39b4f3031dca319b2a28299f245",
 			PayPalEnvironmentSandbox: "access_token$sandbox$v3s48nj697rqm75b$5411d39b4f3031dca319b2a28299f245"
 		}).then(() => {
-			// Environments: PayPalEnvironmentNoNetwork, PayPalEnvironmentSandbox, PayPalEnvironmentProduction
 			this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
-				// Only needed if you get an "Internal Service Error" after PayPal login!
-				//payPalShippingAddressOption: 2 // PayPalShippingAddressOptionPayPal
+
 			})).then(() => {
 				let payment = new PayPalPayment('3.33', 'USD', 'Description', 'sale');
 				this.payPal.renderSinglePaymentUI(payment).then(() => {
 					// Successfully paid
 
-					// Example sandbox response
-					//
-					// {
-					//   "client": {
-					//     "environment": "sandbox",
-					//     "product_name": "PayPal iOS SDK",
-					//     "paypal_sdk_version": "2.16.0",
-					//     "platform": "iOS"
-					//   },
-					//   "response_type": "payment",
-					//   "response": {
-					//     "id": "PAY-1AB23456CD789012EF34GHIJ",
-					//     "state": "approved",
-					//     "create_time": "2016-10-03T13:33:33Z",
-					//     "intent": "sale"
-					//   }
-					// }
 				}, () => {
 					// Error or render dialog closed without being successful
 				});
